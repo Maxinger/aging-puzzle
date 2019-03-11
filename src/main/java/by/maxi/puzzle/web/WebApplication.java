5af8c2643bbed4cbd2ca7d8e409d5698dc88be51
max.kishkevich <max.kishevich@itibo.com>
2019-03-11 11:16:35 +0300

    Experimental template based on Foundation

---
+++ src/main/java/by/maxi/puzzle/web/WebApplication.java
@@ -0,0 +1,15 @@
+package by.maxi.puzzle.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("by.maxi.puzzle")
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
+
