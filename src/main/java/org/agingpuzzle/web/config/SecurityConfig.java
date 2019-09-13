06722925b3eee8a55049e0e9b0478752861534ab
max.kishkevich <max.kishkevich@itibo.com>
2019-09-13 07:39:36 +0300

    Disabled security cache-control

--- src/main/java/org/agingpuzzle/web/config/SecurityConfig.java
+++ src/main/java/org/agingpuzzle/web/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .headers().disable()
             .authorizeRequests()
                 .antMatchers("/*/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
