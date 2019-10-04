bc80210055af1f69bf87654b63f61ce1f4762f9f
max.kishkevich <max.kishevich@itibo.com>
2019-10-04 20:01:37 +0300

    PuzzleResearch

--- src/main/java/org/agingpuzzle/image/PuzzleResearch.java
+++ src/main/java/org/agingpuzzle/image/PuzzleResearch.java
@@ -27,27 +27,38 @@ public class PuzzleResearch {
             {63, 5, 65, 15, 100, 0}
     };
 
-    private Random rand = new Random();
+    private static final int[][] INNER_OUTWARD_COORDS = new int[][]{
+            {5, 5, 38, 18, 39, 8},
+            {39, 8, 43, 3, 42, -2},
+            {43, -7, 25, -16, 50, -16},
+            {50, -20, 80, -20, 62, -5},
+            {62, -5, 60, 0, 63, 5},
+            {63, 5, 65, 15, 100, 0}
+    };
 
     public static void main(String[] args) throws IOException {
         File outputFile = new File("research.png");
         int size = 300;
         double scale = 1.5;
+        int tab = 1;
 
         double[][] coords = new double[6][6];
+        double[][] outCoords = new double[6][6];
+
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 coords[i][j] = BASE_COORDS[i][j] * scale;
+                outCoords[i][j] = INNER_OUTWARD_COORDS[i][j] * scale;
             }
         }
 
         BufferedImage bi = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = bi.createGraphics();
-
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, size, size);
 
         double x0 = (size - scale * 100) / 2;
         double y0 = x0;
-        int tab = -1;
 
         GeneralPath path = new GeneralPath();
         path.moveTo(x0, y0);
@@ -58,10 +69,28 @@ public class PuzzleResearch {
                     x0 + coords[i][4], y0 + tab * coords[i][5]);
         }
 
-        g2d.setColor(Color.WHITE);
-        g2d.fillRect(0, 0, size, size);
+
         g2d.setColor(Color.BLACK);
         g2d.draw(path);
+
+        // inner
+        coords = outCoords;
+
+        x0 = (size - scale * 100) / 2;
+        y0 = x0;
+
+        path = new GeneralPath();
+        path.moveTo(x0 + coords[0][0], y0 + coords[0][1]);
+
+        for (int i = 0; i < 3; i++) {
+            path.curveTo(x0 + coords[i][0], y0 + tab * coords[i][1],
+                    x0 + coords[i][2], y0 + tab * coords[i][3],
+                    x0 + coords[i][4], y0 + tab * coords[i][5]);
+        }
+
+        g2d.setColor(Color.RED);
+        g2d.draw(path);
+
         ImageIO.write(bi, "PNG", outputFile);
 
     }
