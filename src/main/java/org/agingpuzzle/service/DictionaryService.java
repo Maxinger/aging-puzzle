a41ddb050a9237752cf3d5e1a6986f7d66d9a951
max.kishkevich <max.kishevich@itibo.com>
2019-10-16 20:20:19 +0300

    DictionaryService

--- src/main/java/org/agingpuzzle/service/DictionaryService.java
+++ src/main/java/org/agingpuzzle/service/DictionaryService.java
@@ -7,10 +7,10 @@ import org.agingpuzzle.repo.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
-
-import static java.util.function.Function.identity;
 
 @Slf4j
 @Service
@@ -20,23 +20,31 @@ public class DictionaryService {
     @Autowired
     private MessageRepository messageRepository;
 
-    private Map<String, Message> dictionary;
+    // lang -> type -> key -> value
+    private Map<String, Map<String, Map<String, String>>> dictionary;
 
     private String keyForMessage(Message message) {
         return String.format("%s_%s_%s", message.getType(), message.getKey(), message.getLanguage());
     }
 
     private void loadDictionary() {
-        dictionary = messageRepository.findAllOrderByType()
-                .stream().collect(Collectors.toMap(this::keyForMessage, identity()));
+        dictionary = new TreeMap<>();
+        messageRepository.findAllOrderByType().forEach(msg -> {
+            dictionary
+                    .putIfAbsent(msg.getLanguage(), new TreeMap<>())
+                    .putIfAbsent(msg.getType(), new TreeMap<>())
+                    .putIfAbsent(msg.getKey(), msg.getValue());
+        });
     }
 
-    public String getValue(String type, String keys) {
-        return null;
+    public String getValue(String type, String lang, String keys) {
+        return Arrays.stream(keys.split(","))
+                .map(key -> dictionary.get(lang).get(type).getOrDefault(key, key))
+                .collect(Collectors.joining(", "));
     }
 
-    public Map<String, String> getDictionaryForType(String type) {
-        return null;
+    public Map<String, String> getDictionaryForType(String type, String lang) {
+        return dictionary.get(lang).get(type);
     }
 
     public void updateFromCSV(String csv) {
